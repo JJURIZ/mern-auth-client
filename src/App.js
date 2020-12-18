5b9e5778343636cc504a78a7b6d6d927e89aa0a6
Jeremy <jjuriz@gmail.com>
2020-12-18 15:38:14 -0500

    [App] Created authentication logic.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+// IMPORTS
 import React, { useEffect, useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
-
+import setAuthToken from './utils/setAuthToken'
+// CSS
 import './App.css';
-
+// COMPONENTS
 import Welcome from './components/Welcome'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const user = localStorage.getItem('jwToken');
+  const user = localStorage.getItem('jwtToken');
   return <Route {...rest } render={(props) => {
     return user ? <Component { ...rest } { ...props } /> :
     <Redirect to="/login" />
@@ -19,11 +21,43 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 
 
 function App() {
+  // Set state values
+  const [currentUser, setCurrentUser] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
+useEffect(() => {
+  let token;
+
+  // if there is no token in localStorage, then the user is not authenticated
+  if (!localStorage.getItem('jwtToken')) {
+    setIsAuthenticated(false);
+  } else {
+    token = jwt_decode(localStorage.getItem('jwtToken'));
+    setAuthToken(localStorage.jwtToken);
+    setCurrentUser(token)
+  }
+}, []);
+
+const nowCurrentUser = (userData) => {
+  console.log('nowCurrentUser is here...')
+  setCurrentUser(userData);
+  setIsAuthenticated(true);
+};
+
+const handleLogout = () => {
+  if (localStorage.getItem('jwtToken')) {
+    localStorage.removeItem('jwtToken')
+    setCurrentUser(null)
+    setIsAuthenticated(false);
+  }
+};
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar 
+      handleLogout={handleLogout} 
+      isAuth={isAuthenticated}
+      />
       <Welcome />
       <Footer />
     </div>
