a6a2928bb5194b46aee6825fb24e961fdf63cd54
Jeremy <jjuriz@gmail.com>
2020-12-18 12:41:34 -0500

    [Nav] Created navbar with links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Welcome from './components/Welcome'
 import About from './components/About';
 import Footer from './components/Footer';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Welcome />
       <About />
       <Footer />
+      {/* <Navbar /> */}
     </div>
   );
 }
