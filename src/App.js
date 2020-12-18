6a1f2c202bf095996e558d369796d2a00a23e288
Jeremy <jjuriz@gmail.com>
2020-12-18 15:15:01 -0500

    [App] Created PrivateRoute function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+
 import './App.css';
 
 import Welcome from './components/Welcome'
-import About from './components/About';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const user = localStorage.getItem('jwToken');
+  return <Route {...rest } render={(props) => {
+    return user ? <Component { ...rest } { ...props } /> :
+    <Redirect to="/login" />
+  }} />
+}
+
+
 function App() {
+
+
   return (
     <div className="App">
       <Navbar />
       <Welcome />
-      <About />
       <Footer />
     </div>
   );
