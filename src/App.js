d0f107efe0b50a0172af3632f915b2faf9fd7704
Jeremy <jjuriz@gmail.com>
2020-12-18 12:17:13 -0500

    [About, Footer] Completed components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Welcome from './components/Welcome'
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        We need a name. Maybe 'Operation Hot Mother'. Also, your knee is on my
+        heart. Oh by the way, Doctor said no kissing her on the face for one
+        week. I was like make it two weeks, see if I care! Do you guys know
+        where I could get one of those gold necklaces with the T on it? That's a
+        cross. Across from where?
+      </p>
+    </div>
+  );
+};
+
+export default About;
