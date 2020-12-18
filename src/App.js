b509eb5f09190780cd458e0040f441538e9ddfc5
Jeremy <jjuriz@gmail.com>
2020-12-18 12:50:34 -0500

    [Navbar] Fleshed out navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Welcome from './components/Welcome'
 import About from './components/About';
 import Footer from './components/Footer';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Welcome />
       <About />
       <Footer />
-      {/* <Navbar /> */}
     </div>
   );
 }
